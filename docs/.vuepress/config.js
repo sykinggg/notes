5cbc095e691171a671ea05ef58e259fa40ee20e1
DeanSui <dean@57blocks.com>
2021-08-12 09:20:22 +0800

    样式初步优化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,28 +1,66 @@
-module.exports = {
+const {
+    config
+} = require('vuepress-theme-hope');
+
+const sidebar = require('./configs/sidebar/index');
+const nav = require('./configs/navbar/index');
+
+module.exports = config({
     base: '/notes/',
     dest: 'dist',
-    title: '前端 学习',
     port: 666,
+    title: '前端 学习',
+    description: "君子藏器于身 待时而动 天下有道则见 无道则隐",
     markdown: {
-        lineNumbers: true
+        extendMarkdown: md => {
+            md.set({
+                html: true
+            })
+            md.use(require('markdown-it-katex'))
+        }
     },
-    head: [
-        ['script', { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js' }],
-        ['script', { src: 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.2/jquery.fancybox.min.js' }],
-        ['link', { rel: 'stylesheet', type: 'text/css', href: 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.2/jquery.fancybox.min.css' }],
-        ['link', { rel: 'icon', href: '/favicon.ico' }]
-    ],
-    plugins: [
-        ['vuepress-plugin-code-copy', true],
-    ],
-    serviceWorker: false,
+    chainWebpack: config => {
+        config.resolve.alias.set('core-js/library/fn', 'core-js/features')
+    },
     themeConfig: {
-        repo: '',
-        editLinks: true,
+        author: 'sy',
+        sidebar,
+        nav,
+        iconPrefix: "icon-",
+        docsBranch: 'main',
         docsDir: 'docs',
-        lastUpdated: '上次更新',
-        nav: require('./configs/navbar/index'),
-        sidebar: require('./configs/sidebar/index'),
-        sidebarDepth: 4
-    }
-}
+        docsRepo: 'https://github.com/sykinggg/notes',
+        editLinks: true,
+        prevLinks: true,
+        nextLinks: true,
+        mdEnhance: {
+            align: true,
+            flowchart: true,
+            tex: false,
+            lineNumbers: false,
+            demo: true,
+            mermaid: true
+        },
+        feed: false,
+        hostname: ''
+    },
+    plugins: [
+        "@vuepress/nprogress",
+        "vuepress-plugin-viewer",
+        ['disqus', {
+            shortname: 'sycoder'
+        }],
+        // "code-switcher",
+        "element-ui"
+    ],
+    head: [
+        ['link', {
+            rel: 'stylesheet',
+            href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css'
+        }],
+        ['link', {
+            rel: "stylesheet",
+            href: "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css"
+        }]
+    ]
+})
--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,39 @@
+---
+home: true
+lang: zh-CN
+heroText: 笔记!
+heroImage: /logo.svg
+
+action:
+  - text: 数据结构 💡
+    type: primary
+
+  - text: ES6
+
+features:
+  - title: 复合面试题
+    details: 面试题的一部分汇总
+    link: /algorithm/interview/baseInterview/
+  - title: mozilla-js
+    details: mozilla js 文档
+    link: /mozillajs/baseObject/symbol/
+  - title: mozilla-css
+    details: mozilla css 文档
+    link: /mozillaCss/reference/universalSelectors/
+  - title: js 概念
+    details: js 基础概念
+    link: /js/stack/executionStack/
+  - title: css
+    details: css 面试题
+    link: /css/interview/baseCss/
+  - title: html
+    details: html 面试题
+    link: /html/interview/linkImport/
+  - title: 浏览器 面试题
+    details: 浏览器 面试题
+    link: /browser/interview/baseInterview/
+---
+
 * 复合面试题
     * [第一套综合基础面试题](/algorithm/interview/baseInterview.md)
 
