73f736c7f4e75c3372b9d6bd118a96eeb6cd9129
suiyi <suiyi19920228@qq.com>
2022-01-08 10:09:18 +0800

    update version

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,8 +4,8 @@ const nav = require('./nav');
 
 module.exports = {
   lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
+  title: '笔记!',
+  description: '君子藏器于身 待时而动 天下有道则见 无道则隐',
   base: '/notes/',
 
   themeConfig: {
